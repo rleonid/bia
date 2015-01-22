50edde35fb30179a2faffab7006009ff9a6e01b6
Leonid Rozenberg <leonidr@gmail.com>
2015-01-22 12:35:39 -0500

    Smarter algorithms.

--- src/lib/skew.ml
+++ src/lib/skew.ml
@@ -1,40 +1,41 @@
-open Core.Std
+let update_skew_count c = function
+    | 'C' -> (c - 1)
+    | 'G' -> (c + 1)
+    | 'A'
+    | 'T' -> c
+    | x   -> raise (Invalid_argument (Printf.sprintf "skew: %c" x))
 
+(* Notes
+ * - Not a generic implementation as it is tailored for computing C - G.
+ * - Includes the 0 starting value.
+ *)
 let skew str =
   let n = String.length str in
-  let rec loop i c acc =
-    if i >= n then
-      List.rev (c :: acc)
-    else
-      let nc = 
-        match str.[i] with 
-        | 'C' -> (c - 1)
-        | 'G' -> (c + 1) 
-        | 'A' | 'T' -> c
-        | x   -> raise (Invalid_argument (sprintf "skew: %c" x))
-      in
-      loop (i + 1) nc (c :: acc)
-  in
-  loop 0 0 []
-
+  let a = Array.make (n + 1) 0 in
+  for i = 1 to n do
+    a.(i) <- update_skew_count a.(i-1) str.[i-1];
+  done;
+  a
 
 let skew_min str =
-  let sa = skew str in
-  let ma = List.mapi ~f:(fun i s -> (i, s)) sa in
-  let so = List.sort ~cmp:(fun (_,s1) (_,s2) -> compare s1 s2) ma in
-  let (_, m) = List.hd_exn so in
-  let rec loop acc = function
-    | []           -> List.rev acc
-    | (i,m2) :: tl -> if m2 = m 
-                      then loop (i :: acc) tl
-                      else List.rev acc
+  let n = String.length str in
+  let rec loop count min_count pos_lst i =
+    if i >= n then
+      (min_count, pos_lst)
+    else
+      let new_count = update_skew_count count str.[i] in
+      if new_count < min_count then
+        loop new_count new_count [i+1] (i + 1)
+      else if new_count = min_count then
+        loop new_count min_count (i+1::pos_lst) (i + 1)
+      else
+        loop new_count min_count pos_lst (i + 1)
   in
-  (m, loop [] so)
-
+  loop 0 0 [0] 0
 
 (* Tests *)
 let test_skew () =
-  assert (skew "ACGGC" =  [0; 0; -1; 0; 1; 0])
+  assert (skew "ACGGC" =  [|0; 0; -1; 0; 1; 0|])
 
 let test_skew_min () =
   assert (skew_min "ACGGC" = (-1,[2]))
