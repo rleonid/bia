d1ba265cbe44a266b9ffd0f2dd5f2f5fd4512572
Leonid Rozenberg <leonidr@gmail.com>
2015-01-26 21:28:52 -0500

    Moving the skew functionality into Genomes module.

--- src/lib/genomes.mli
+++ src/lib/genomes.mli
@@ -5,3 +5,13 @@ val acc_upper : (int * int * int * int) -> char -> (int * int * int * int)
     of the four nucleotides. *)
 
 val counts_upper : string -> (int * int * int * int)
+(** [counts_upper genome] returns a quadruple of the number of times
+    'A','C','G' and 'T' are found in [genome]. *)
+
+val skew : string -> int array
+(** [skew sequence] returns the difference between the number of observe 'G' and
+    'C' nucleotydes. *)
+
+val skew_min : string -> (int * int list)
+(** [skew_min] computes the value and locations of minimum skew in the passed
+    sequence. *)
--- src/lib/skew.mli
+++
@@ -1,8 +0,0 @@
-
-val skew : string -> int array
-(** [skew sequence] returns the difference between the number of observe 'G' and
-    'C' nucleotydes. *)
-
-val skew_min : string -> (int * int list)
-(** [skew_min] computes the value and locations of minimum skew in the passed
-    sequence. *)
