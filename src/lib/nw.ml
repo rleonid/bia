a33735d04d34c8378d5b86c15ec823d64c7769e8
Leonid Rozenberg <leonidr@gmail.com>
2015-03-06 01:26:16 -0500

    Using functional style.

--- src/lib/nw.ml
+++ src/lib/nw.ml
@@ -1,92 +1,67 @@
 
-type path = Match
-          | Delete
-          | Insert
-          | MatDel
-          | MatIns
-          | DelIns
-          | AllThree
+type dir = Left
+         | TopLeft
+         | Top
 
-let tmax ~mat ~del ~ins =
-  let comp2 x y eq lt gt = 
-    if x = y
-    then eq, x
-    else if x < y
-         then lt, y
-         else gt, x
+let tmax ~tl ~l ~t =
+  let against_top d ds =
+    if d = t
+    then (Top :: ds), d
+    else if d < t
+         then [Top], t
+         else ds, d
   in
-  if mat = del
-  then comp2 del ins AllThree Insert MatDel
-  else if mat < del
-       then comp2 del ins DelIns Insert Delete
-       else (* mat > del *)
-            comp2 mat ins MatIns Insert Match
+  if tl = l
+  then against_top l [Left; TopLeft]
+  else if tl < l
+       then against_top l [Left]
+       else against_top tl  [TopLeft]
 
+(* Construct the Needleman-Wunsch grid *)
 let needleman_wunsch_grid gap p s1 s2 =
   let x = String.length s1
   and y = String.length s2 in
-  let m = ArrayLabels.make_matrix ~dimx:(x+1) ~dimy:(y+1) 0 in
-  let tmax x y z = if x > y then max x z else max y z in
-  for i = 1 to x do m.(i).(0) <- gap * i done;
-  for j = 1 to y do m.(0).(j) <- gap * j done;
+  let m = ArrayLabels.make_matrix ~dimx:(x+1) ~dimy:(y+1) ([TopLeft], 0) in
+  for i = 1 to x do m.(i).(0) <- ([Top], gap * i) done;
+  for j = 1 to y do m.(0).(j) <- ([Left], gap * j) done;
   for i = 1 to x do
     for j = 1 to y do
       let i1 = i - 1
       and j1 = j - 1 in
-      let mat = m.(i1).(j1) + p s1.[i1] s2.[j1]
-      and del = m.(i1).(j) + gap
-      and ins = m.(i).(j1) + gap in
-      m.(i).(j) <- tmax mat del ins
+      let tl = (snd m.(i1).(j1)) + p s1.[i1] s2.[j1]
+      and t  = (snd m.(i1).(j)) + gap
+      and l  = (snd m.(i).(j1)) + gap in
+      m.(i).(j) <- tmax ~tl ~t ~l
     done;
   done;
-  m
-
-let rec take f = function
-  | []     -> []
-  | h :: t -> if f h 
-              then h :: take f t
-              else []
+  m, m.(x).(y)
 
 let traceback m row col =
-  let make_strings n (l, r) =
-    let ll = String.make n '-'
-    and rr = String.make n '-' in
-    let _n = List.fold_left2 (fun idx lc rc -> 
-                ll.[idx] <- lc;
-                rr.[idx] <- rc;
-                idx + 1) 0 l r
-    in
-    (ll, rr)
-  in
   let add cl cr (ll,rr) = cl::ll, cr::rr in
   let ladd cl cr = List.map (add cl cr) in
-  let rec loop i j n acc =
-    match (i, j) with
-    | (0, 0) -> List.map (make_strings n) acc
-    | (0, _) -> loop 0 (j - 1) (n + 1) (ladd '-' col.[j-2] acc)
-    | (_, 0) -> loop (i - 1) 0 (n + 1) (ladd row.[i-2] '-' acc)
-    | _      ->
-        let i1 = i - 1 and j1 = j - 1 in
-        let rc = row.[i1] and cc = col.[j1] in
-        (*Printf.printf "-%d %d\n" i j; *)
-        let weights = 
-          [ ((i1,j1), (rc,  cc)); 
-            ((i1,j),  (rc,  '-'));
-            ((i,j1),  ('-', cc)) ] 
-          |> List.map (fun ((x,y) as p,c) -> (m.(x).(y), p, c))
-          |> List.sort (fun (w1, _, _) (w2, _, _) -> -1 * compare w1 w2)
-        in
-        let w, _, _ = List.hd weights in
-        take (fun (w1,_, _) -> w1 = w) weights
-        |> List.map (fun (_, (x,y), (r,c)) -> loop x y (n + 1) (ladd r c acc))
-        |> List.flatten 
+  let rec loop n i j acc =
+    if i = 0 && j = 0
+    then List.map (make_strings n) acc
+    else
+      let i1 = i - 1 and j1 = j - 1 in
+      let dirs, _ = m.(i).(j) in
+      dirs
+      |> List.map (function
+            | Left    -> loop (n + 1) i  j1 (ladd '-'      col.[j1] acc)
+            | Top     -> loop (n + 1) i1 j  (ladd row.[i1] '-'      acc)
+            | TopLeft -> loop (n + 1) i1 j1 (ladd row.[i1] col.[j1] acc))
+      |> List.flatten
   in
-  loop (Array.length m - 1) (Array.length m.(0) - 1) 0 [[],[]]
-       
+  loop 0 (Array.length m - 1) (Array.length m.(0) - 1) [[],[]]
 
-let can_nw = needleman_wunsch_grid (-1) (fun x y -> if x = y then 1 else -1)
-let s1 = "ACTG"
-let s2 = "ACTG"
-let t1 = "GATTACA" 
-let t2 = "GCATGCU" 
-let t3 = "GCATGCUG" 
+let nw gap p s1 s2 =
+  let m, (_, score) = needleman_wunsch_grid gap p s1 s2 in
+  traceback m s1 s2, score
+
+(* Canonical values. *)
+let c_gap = -1
+let c_score x y = if x = y then 1 else -1
+
+(* Canonical Needleman-Wunsch *)
+let c_nw = nw canonical_gap canonical_score
+
